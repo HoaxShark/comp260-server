2b43fc47398d6819e90bd15fe4515fe41a0d8b0a
hoaxshark <hoaxshark@live.com>
2019-01-29 15:57:41 +0000

    added some child classes to the items class. started work on inventory and equipped items.

--- Scripts/item.py
+++ Scripts/item.py
@@ -6,7 +6,27 @@ class Item:
     max_durability = ''
 
     def __init__(self, name, weight, max_durability):
-        name = name
-        weight = weight
-        max_durability = max_durability
-        durability = max_durability
+        self.name = name
+        self.weight = weight
+        self.max_durability = max_durability
+        self.durability = max_durability
+
+
+class Weapon(Item):
+
+    damage = ''
+
+    def __init__(self, name, weight, max_durability, damage):
+        super().__init__(name, weight, max_durability)
+        self.damage = damage
+
+
+class Armour(Item):
+
+    defence = ''
+    body_part = ''
+
+    def __init__(self, name, weight, max_durability, defence, body_part):
+        super().__init__(name, weight, max_durability)
+        self.defence = defence
+        self.body_part = body_part
--- Scripts/player.py
+++ Scripts/player.py
@@ -1,9 +1,11 @@
 class Player:
 
+    player_id = ''  # Identification tag of the player
     current_room = ''  # Room the player is currently in
     current_weight = ''  # The current weight of everything the player has
     max_weight = ''  # Maximum weight the player can reach before being unable to move
-    player_id = ''  # Identification tag of the player
+    inventory = []  # Items the player has on them
+    equipped = []  # Items the player had equipped
 
     def __init__(self, start_room):
         self.current_room = start_room
