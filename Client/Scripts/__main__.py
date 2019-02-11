8bae0c38ff3f68ce4e3e058206a54835e3d9ec93
hoaxshark <hoaxshark@live.com>
2019-02-11 12:57:37 +0000

    improved robustness to client and server,

--- Client/Scripts/__main__.py
+++ Client/Scripts/__main__.py
@@ -7,12 +7,10 @@ import socket
 
 # Entry point of programme
 def main():
-    # create a socket
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # connect to the IP address using port
-    my_socket.connect(("127.0.0.1", 8222))
-    connected = True
-    print("Connected to Server.")
+
+    is_connected = False
+    my_socket = None
+    is_running = True
 
     input_manager = input.Input(my_socket)
 
@@ -24,25 +22,32 @@ def main():
     #threading_2.daemon = True
     #threading_2.start()
 
-    while True:
-        # send user input to the server then wait for data back
-        try:
-            input_manager.player_input(my_socket)
-            receive_data(my_socket)
+    while is_running:
+        # if not connected loop here trying to connect until success
+        while not is_connected:
+            if my_socket == None:
+                # create a socket
+                my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            try:
+                # connect to the IP address using port
+                my_socket.connect(("127.0.0.1", 8222))
+                is_connected = True
+                print("Connected to Server.")
+            except socket.error:
+                is_connected = False
+                print("no luck connecting, try again in 2 secs")
+                sleep(2)
 
-        except socket.error:
-            print("Server Lost. Will try and reconnect.")
-            connected = False
-            while not connected:
-                # try to reconnect, else sleep 2 seconds try again
-                try:
-                    my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    my_socket.connect(("127.0.0.1", 8222))
-                    connected = True
-                    print("Re-connection successful")
-                except socket.error:
-                    print("no luck connecting, try again in 2 secs")
-                    sleep(2)
+        # main play loop sends and receives data to server
+        while is_connected:
+            try:
+                input_manager.player_input(my_socket)
+                receive_data(my_socket)
+
+            except socket.error:
+                print("Server Lost. Will try and reconnect.")
+                my_socket = None
+                is_connected = False
 
     print('Exiting Dungeon')
     my_socket.close()
--- Server/Scripts/__main__.py
+++ Server/Scripts/__main__.py
@@ -1,10 +1,19 @@
 import socket
+import threading
 
 from Scripts import input
 from Scripts import player
 from Scripts import dungeon
 
+# dictionary of all connected clients
+clients = {}
+# lock for protecting the client dictionary
+clientsLock = threading.Lock()
+
 if __name__ == '__main__':
+
+    is_running = True
+    is_connected = False
 
     # create the socket
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -12,15 +21,19 @@ if __name__ == '__main__':
     my_socket.bind(("127.0.0.1", 8222))
     # listen for new connections / waits here until a client connects
     my_socket.listen(5)
-    # new client has connected
-    client = my_socket.accept()
 
     # generate dungeon
     my_dungeon = dungeon.Dungeon()
     my_player = player.Player(my_dungeon, 'Hall')
     input_manager = input.Input()
 
-    while True:
+    while is_running:
+        while not is_connected:
+            print("Waiting for client.")
+            # new client has connected
+            client = my_socket.accept()
+            print("Client connected.")
+            is_connected = True
         try:
             # receive data from client
             data = client[0].recv(4096)
@@ -33,7 +46,7 @@ if __name__ == '__main__':
 
         except socket.error:
             print("Client Lost")
-            # listen for new connections / waits here until a client connects
-            my_socket.listen(5)
-            # new client has connected
-            client = my_socket.accept()
+            is_connected = False
+
+def accept_client(server_socket):
+    # do server connections here
