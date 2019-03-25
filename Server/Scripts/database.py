b8c1f9a8a5a2628c7456ae51a14be19b16dcd8a4
hoaxshark <hoaxshark@live.com>
2019-03-25 17:24:09 +0000

    started the server database class

---
+++ Server/Scripts/database.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+
+class Database:
+
+    def __init__(self):
+        # create or open a file called my_db
+        self.db = sqlite3.connect('server_db')
+        # get cursor object
+        self.cursor = self.db.cursor()
+
+    def create_user_table(self):
+        self.cursor.execute('''
+                        CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, salt TEXT)
+                        ''')
+        # commit the change
+        self.db.commit()
+        print('Table created')
+
+    # Adds a new user to the db
+    def add_user(self, name, password, salt):
+        # Insert user
+        self.cursor.execute('''INSERT INTO users(username, password, salt)
+                          VALUES(?,?,?)''', (name, password, salt))
+        self.db.commit()
+        print('User inserted')
+
+    # Returns the salt for the password encryption
+    def get_salt(self, name):
+        self.cursor.execute('''SELECT salt FROM users WHERE username=?''', (name,))
+        salt = self.cursor.fetchone()  # retrieve the first row
+        return salt
+
+    # Checks the db for a matching username returns 0 if it doesn't exist and 1 if it does
+    def check_name(self, name):
+        self.cursor.execute('''SELECT username FROM users WHERE username=?''', (name,))
+        user = self.cursor.fetchone()  # retrieve the first row
+        if user is None:
+            return 0
+        elif user is not None:
+            return 1
+
+    # Checks the db for a matching password returns 0 if it doesn't match and 1 if it does
+    def check_password(self, name, password):
+        self.cursor.execute('''SELECT password FROM users WHERE username=?''', (name,))
+        db_password = self.cursor.fetchone()  # retrieve the first row
+        if db_password == password:
+            return 1
+        elif db_password != password:
+            return 0
+
+    # Update the users password
+    def change_password(self, name, password):
+        # Insert user
+        self.cursor.execute('''UPDATE users SET password =? WHERE name = ? ''', (password, name))
+        self.db.commit()
+        print('Password updated')
